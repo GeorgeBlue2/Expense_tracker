5059c3dece167359632615ceff720d0250ec8134
George Bluemke <georgebluemke2@gmail.com>
2025-11-08 21:55:18 -0500

    Fix file name and enable user input for expenses
    Updated the expense tracker to read from 'expense.csv' and allow user input for new expenses.

--- expense_tracker.py
+++ expense_tracker.py
@@ -4,8 +4,8 @@ from datetime import datetime
 
 # Checks for existing data
 
-if os.path.exists('expenses.scv'):
-    df = pd.read_csv('expenses.csv')
+if os.path.exists('expense.csv'):
+    df = pd.read_csv('expense.csv')
     print(f"Loaded existing expense: {len(df)} rows")
     print("Here's what's loaded")
     print(df)
@@ -20,15 +20,23 @@ else:
 print("\nCurrent expenses: ")
 print(df)
 
+# Gets user input
+
+print("\n--- Add New Expense ---")
+category = input("Category (Food/Transport/Entertainment/etc): ")
+amount = float(input("Amount: $"))
+description = input("Description: ")
+
+
 # Add a new expense
 new_expense = pd.DataFrame([{
     'date': datetime.now().strftime('%Y-%m-%d'),
-    'category':'Food',
-    'amount': 15.50,
-    'description': 'Lunch'
+    'category':category,
+    'amount': amount,
+    'description': description
     }])
 
-# df = pd.concat([df, new_expense], ignore_index=True)
+df = pd.concat([df, new_expense], ignore_index=True)
 
 print("\nCurrent expenses AFTER adding: ")
 print(df)
